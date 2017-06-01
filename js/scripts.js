94ef6bb7e813b0264b77b642497bb6723e7f384e
jacob ruleaux and Nathan Stewart <jacob@>
2017-06-01 11:01:32 -0700

    add buttons for 3 animals in index.html, make platypus button clickable to reveal pic and text in html and js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("button#platy").click(function() {
+    $("div.plat").show();
+
+  //  var platbutton = button#platy;
+
+  //  if (platbutton) {
+  //    $(".plat").show();
+  //  }
+  });
+});
