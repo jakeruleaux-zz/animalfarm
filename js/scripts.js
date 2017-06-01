18f7a1623373376aa52245c25d4ce8fa42fe84b8
jacob ruleaux and Nathan Stewart <nathan@>
2017-06-01 11:12:04 -0700

    add javascript and css for last two btns

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $("button#platy").click(function() {
     $("div.plat").show();
-
-  //  var platbutton = button#platy;
-
-  //  if (platbutton) {
-  //    $(".plat").show();
-  //  }
+  });
+  $("button#coot").click(function() {
+    $("div.coot").show();
+  });
+  $("button#narwhal").click(function() {
+    $("div.nar").show();
   });
 });
